5aa340efabb4759eca9eeca915b1840b4e2f76fb
Sergio Gomis <sgomis@sonneil.com>
2020-02-10 15:01:01 +0100

    lunes

--- src/gameslib.py
+++ src/gameslib.py
@@ -1,2 +1,27 @@
+import math
+import requests
+
+
 #libreria aux
 
+def geocode(address):
+    data = requests.get(f"https://geocode.xyz/{address}?json=1").json()
+    return {
+        "type":"Point",
+        "coordinates":[float(data["longt"]),float(data["latt"])]
+    }
+
+
+def gMapsLink(point):
+    return f'https://www.google.com/maps/@{point["coordinates"][1]},{point["coordinates"][0]},14z'
+
+
+def toCartoPoint(lon,lat):
+    return f'POINT ({str(lon)} {str(lat)})'
+
+
+def toGeoJSON(lon,lat):
+    return {
+        "type":"Point",
+        "coordinates":[lon,lat]
+    }
